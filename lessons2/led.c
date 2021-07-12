c6849d80dbd5754beb39267c1273af0352d587ad
2761383195 <87225072+2761383195@users.noreply.github.com>
2021-07-12 11:57:48 +0800

    代码
    流水灯的代码

--- lessons2/led.c
+++ lessons2/led.c
@@ -1,1 +1,28 @@
+// C++ code
+//
+int i=13;
+void setup()
+{
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(7, OUTPUT);
+}
 
+void loop()
+{
+  while(i>6){
+  digitalWrite(i, HIGH);
+    
+  delay(1000); // Wait for 1000 millisecond(s)
+    digitalWrite(i, LOW);
+     delay(1000);
+    i--;
+    if(i<=6){
+      i=13;
+    }
+  }
+}
